5f626e1e2facfbd11d1ed35008efbc2436251110
TalissonMelo <talissonmelorodrigues@gmail.com>
2022-02-14 20:49:47 -0300

    Criando tabela

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const customExpress = require('./configuracao/customExpress');
 const conexao = require('./infraestrutura/conexao');
+const tabelas = require('./infraestrutura/tabelas')
+
 
 conexao.connect(error => {
     if(error) {
         console.log(error);
     } else {
         console.log("CONECTADO COM SUCESSO AO BANCO DE DADOS PET!.")
+        tabelas.init(conexao)
         const app = customExpress();
         app.listen(3000, () => {
             console.log("Aplicação iniciada")
